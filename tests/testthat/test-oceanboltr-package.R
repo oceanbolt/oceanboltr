94166fb8bab4149c3083fb7627612195ef95cf3f
Alexander Gedranovich <alexander@epica.ai>
2020-12-11 14:52:21 +0300

    Tests for the package loading

--- tests/testthat/test-oceanboltr-package.R
+++ tests/testthat/test-oceanboltr-package.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Package loads correctly", {
+  expect_silent(library(oceanboltr))
+  expect_null(oceanboltr:::.onLoad())
+  expect_equal(class(Sys.getenv("OCEANBOLT_TOKEN")), "character")
+  expect_true({
+    if (requireNamespace("keyring", quietly = TRUE)) {
+      keys <- suppressWarnings(keyring::key_list("OCEANBOLT_TOKEN"))
+      "service" %in% names(keys)
+    } else {
+      TRUE
+    }
+  })
 })
